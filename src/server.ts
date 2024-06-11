b3458ebf3c21d6cabdcd859b84ddff23de9eca16
Jaedsonn <128820369+Jaedsonn@users.noreply.github.com>
2024-06-10 21:28:20 -0300

    Commit inicial

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from "express"
+import { Request, Response, Express } from "express"
+import routes from "./routes/routes";
+
+const server: Express = express();
+const PORT = 3001;
+
+server.use(express.json())
+
+server.use("/usuario", routes)
+
+server.get("/", (_req: Request, res:Response )=>{
+    res.send("Primeira rota")
+})
+
+server.listen(PORT, ()=>{
+    console.log(`Servidor rodando em: http://localhost:${PORT}`)
+})
